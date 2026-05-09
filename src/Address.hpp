3f58768b6b6d13da3cc37067af16aaeda135cf04
Mateja Miletic <miletic.matejamiletic.mateja@gmail.com>
2026-05-09 20:05:24 +0200

    feat(contact): implemented Address class

---
+++ src/Address.hpp
@@ -0,0 +1,24 @@
+#include <string>
+
+class Address {
+private:
+    std::string street;
+    int postalCode = 0;
+    std::string city;
+    std::string country;
+
+public:
+    Address(const std::string& street, int postalCode, const std::string& city, const std::string& country);
+    Address()  = default;
+   ~Address()  = default;
+    
+    std::string getStreet() const;
+    int getPostalCode() const;
+    std::string getCity() const;
+    std::string getCountry() const;
+
+    std::string format() const;
+    bool isEmpty() const;
+
+    bool operator==(const Address& other) const;
+};
