ccf85c0845499a6302e3ef15acbffab07f7a384d
Mateja Miletic <miletic.matejamiletic.mateja@gmail.com>
2026-05-16 21:01:25 +0200

    test(vcf-parser-and-others): complete tests for VcfParser, Address, Contact and PhoneNumber

---
+++ tests/phone_number/address.test.cpp
@@ -0,0 +1,126 @@
+#include <catch2/catch_test_macros.hpp>
+#include <phone_number/address.hpp>
+
+TEST_CASE("Address default constructor creates empty address", "[Address]") {
+    Address a;
+    REQUIRE(a.getStreet().empty());
+    REQUIRE(a.getPostalCode().empty());
+    REQUIRE(a.getCity().empty());
+    REQUIRE(a.getCountry().empty());
+    REQUIRE(a.isEmpty());
+}
+
+TEST_CASE("Address constructor stores all fields correctly", "[Address]") {
+    Address a("21 Main St", "10000", "Novi Sad", "Serbia");
+    REQUIRE(a.getStreet()     == "21 Main St");
+    REQUIRE(a.getPostalCode() == "10000");
+    REQUIRE(a.getCity()       == "Novi Sad");
+    REQUIRE(a.getCountry()    == "Serbia");
+    REQUIRE_FALSE(a.isEmpty());
+}
+
+TEST_CASE("Address partial construction", "[Address]") {
+    SECTION("only street") {
+        Address a("21 Main St");
+        REQUIRE(a.getStreet() == "21 Main St");
+        REQUIRE(a.getCity().empty());
+        REQUIRE_FALSE(a.isEmpty());
+    }
+    SECTION("only city") {
+        Address a("", "", "Novi Sad");
+        REQUIRE(a.getCity() == "Novi Sad");
+        REQUIRE(a.getStreet().empty());
+        REQUIRE_FALSE(a.isEmpty());
+    }
+    SECTION("only postal code") {
+        Address a("", "10000");
+        REQUIRE(a.getPostalCode() == "10000");
+        REQUIRE_FALSE(a.isEmpty());
+    }
+    SECTION("only country") {
+        Address a("", "", "", "Serbia");
+        REQUIRE(a.getCountry() == "Serbia");
+        REQUIRE_FALSE(a.isEmpty());
+    }
+}
+
+TEST_CASE("Address isEmpty", "[Address]") {
+    SECTION("all fields empty returns true") {
+        REQUIRE(Address{}.isEmpty());
+    }
+    SECTION("each individual field set returns false") {
+        REQUIRE_FALSE(Address("street").isEmpty());
+        REQUIRE_FALSE(Address("", "10000").isEmpty());
+        REQUIRE_FALSE(Address("", "", "city").isEmpty());
+        REQUIRE_FALSE(Address("", "", "", "country").isEmpty());
+    }
+}
+
+TEST_CASE("Address getFormatted", "[Address]") {
+    SECTION("empty address returns empty string") {
+        REQUIRE(Address{}.getFormatted().empty());
+    }
+    SECTION("full address formats correctly") {
+        Address a("21 Main St", "10000", "Novi Sad", "Serbia");
+        REQUIRE(a.getFormatted() == "21 Main St, 10000 Novi Sad, Serbia");
+    }
+    SECTION("no street") {
+        Address a("", "10000", "Novi Sad", "Serbia");
+        REQUIRE(a.getFormatted() == "10000 Novi Sad, Serbia");
+    }
+    SECTION("no postal code") {
+        Address a("21 Main St", "", "Novi Sad", "Serbia");
+        REQUIRE(a.getFormatted() == "21 Main St, Novi Sad, Serbia");
+    }
+    SECTION("no city") {
+        Address a("21 Main St", "10000", "", "Serbia");
+        REQUIRE(a.getFormatted() == "21 Main St, 10000 , Serbia");
+    }
+    SECTION("no country") {
+        Address a("21 Main St", "10000", "Novi Sad", "");
+        REQUIRE(a.getFormatted() == "21 Main St, 10000 Novi Sad");
+    }
+    SECTION("only country") {
+        Address a("", "", "", "Serbia");
+        REQUIRE(a.getFormatted() == "Serbia");
+    }
+    SECTION("only street and country") {
+        Address a("21 Main St", "", "", "Serbia");
+        REQUIRE(a.getFormatted() == "21 Main St, Serbia");
+    }
+}
+
+TEST_CASE("Address equality operator", "[Address]") {
+    Address a("21 Main St", "10000", "Novi Sad", "Serbia");
+    Address b("21 Main St", "10000", "Novi Sad", "Serbia");
+    Address c("5 Other St", "20000", "Belgrade", "Serbia");
+
+    SECTION("equal addresses") {
+        REQUIRE(a == b);
+    }
+    SECTION("different addresses") {
+        REQUIRE_FALSE(a == c);
+    }
+    SECTION("different only in street") {
+        Address d("99 Other St", "10000", "Novi Sad", "Serbia");
+        REQUIRE_FALSE(a == d);
+    }
+    SECTION("different only in postal code") {
+        Address d("21 Main St", "99999", "Novi Sad", "Serbia");
+        REQUIRE_FALSE(a == d);
+    }
+    SECTION("different only in city") {
+        Address d("21 Main St", "10000", "Belgrade", "Serbia");
+        REQUIRE_FALSE(a == d);
+    }
+    SECTION("different only in country") {
+        Address d("21 Main St", "10000", "Novi Sad", "Germany");
+        REQUIRE_FALSE(a == d);
+    }
+    SECTION("empty equals empty") {
+        REQUIRE(Address{} == Address{});
+    }
+    SECTION("empty does not equal non-empty") {
+        REQUIRE_FALSE(Address{} == a);
+    }
+}
---
+++ tests/phone_number/vcf_parser.test.cpp
@@ -0,0 +1,268 @@
+#include <catch2/catch_test_macros.hpp>
+#include <phone_number/vcf_parser.hpp>
+#include <phone_number/phone_normalizer.hpp>
+#include <filesystem>
+#include <fstream>
+#include <string>
+
+namespace {
+class TempVcfFile {
+  public:
+    explicit TempVcfFile(const std::string &content) {
+        static int counter = 0;
+        path = std::filesystem::temp_directory_path() /
+               ("vcf_parser_test_" + std::to_string(counter++) + ".vcf");
+        std::ofstream out(path);
+        out << content;
+    }
+    ~TempVcfFile() {
+        std::error_code ec;
+        std::filesystem::remove(path, ec);
+    }
+    std::string filename() const { return path.string(); }
+
+  private:
+    std::filesystem::path path;
+};
+
+class TempMetaFile {
+  public:
+    TempMetaFile() {
+        static int counter = 0;
+        path = std::filesystem::temp_directory_path() /
+               ("vcf_meta_test_" + std::to_string(counter++) + ".json");
+        std::ofstream out(path);
+        out << R"json({
+            "countries": {
+                "RS": ["381", "00", "0"],
+                "US": ["1",   "011", "1"],
+                "DE": ["49",  "00",  "0"]
+            }
+        })json";
+    }
+    ~TempMetaFile() {
+        std::error_code ec;
+        std::filesystem::remove(path, ec);
+    }
+    std::string filename() const { return path.string(); }
+
+  private:
+    std::filesystem::path path;
+};
+} 
+
+TEST_CASE("VcfParser loads a single contact", "[VcfParser]") {
+    TempMetaFile meta;
+    TempVcfFile vcf(
+        "BEGIN:VCARD\r\n"
+        "VERSION:3.0\r\n"
+        "N:Petrovic;Marko;;;\r\n"
+        "FN:Marko Petrovic\r\n"
+        "TEL:+38166123456\r\n"
+        "EMAIL:marko@email.com\r\n"
+        "END:VCARD\r\n"
+    );
+
+    PhoneNormalizer normalizer(meta.filename());
+    VcfParser parser(normalizer, "RS");
+
+    auto contacts = parser.loadFromFile(vcf.filename());
+
+    REQUIRE(contacts.size() == 1);
+    REQUIRE(contacts[0].getName()    == "Marko");
+    REQUIRE(contacts[0].getSurname() == "Petrovic");
+    REQUIRE(contacts[0].getEmail()   == "marko@email.com");
+    REQUIRE(contacts[0].hasPhoneNumbers());
+    REQUIRE(contacts[0].getPrimaryPhoneNumber().getNormalizedValue() == "+38166123456");
+}
+
+TEST_CASE("VcfParser loads multiple contacts", "[VcfParser]") {
+    TempMetaFile meta;
+    TempVcfFile vcf(
+        "BEGIN:VCARD\r\n"
+        "VERSION:3.0\r\n"
+        "N:Petrovic;Marko;;;\r\n"
+        "FN:Marko Petrovic\r\n"
+        "TEL:+38166123456\r\n"
+        "END:VCARD\r\n"
+        "BEGIN:VCARD\r\n"
+        "VERSION:3.0\r\n"
+        "N:Jovic;Jovana;;;\r\n"
+        "FN:Jovana Jovic\r\n"
+        "TEL:+38166999999\r\n"
+        "END:VCARD\r\n"
+    );
+
+    PhoneNormalizer normalizer(meta.filename());
+    VcfParser parser(normalizer, "RS");
+
+    auto contacts = parser.loadFromFile(vcf.filename());
+
+    REQUIRE(contacts.size() == 2);
+    REQUIRE(contacts[0].getName() == "Marko");
+    REQUIRE(contacts[1].getName() == "Jovana");
+}
+
+TEST_CASE("VcfParser normalizes local phone number", "[VcfParser]") {
+    TempMetaFile meta;
+    TempVcfFile vcf(
+        "BEGIN:VCARD\r\n"
+        "VERSION:3.0\r\n"
+        "N:Petrovic;Marko;;;\r\n"
+        "TEL:066123456\r\n"
+        "END:VCARD\r\n"
+    );
+
+    PhoneNormalizer normalizer(meta.filename());
+    VcfParser parser(normalizer, "RS");
+
+    auto contacts = parser.loadFromFile(vcf.filename());
+
+    REQUIRE(contacts.size() == 1);
+    REQUIRE(contacts[0].getPrimaryPhoneNumber().getNormalizedValue() == "+38166123456");
+}
+
+TEST_CASE("VcfParser loads contact with address", "[VcfParser]") {
+    TempMetaFile meta;
+    TempVcfFile vcf(
+        "BEGIN:VCARD\r\n"
+        "VERSION:3.0\r\n"
+        "N:Petrovic;Marko;;;\r\n"
+        "TEL:+38166123456\r\n"
+        "ADR:;;21 Main St;Novi Sad;;10000;Serbia\r\n"
+        "END:VCARD\r\n"
+    );
+
+    PhoneNormalizer normalizer(meta.filename());
+    VcfParser parser(normalizer, "RS");
+
+    auto contacts = parser.loadFromFile(vcf.filename());
+
+    REQUIRE(contacts.size() == 1);
+    REQUIRE(contacts[0].hasAddress());
+    REQUIRE(contacts[0].getAddress().getCity()    == "Novi Sad");
+    REQUIRE(contacts[0].getAddress().getCountry() == "Serbia");
+    REQUIRE(contacts[0].getAddress().getStreet()  == "21 Main St");
+}
+
+TEST_CASE("VcfParser loads contact with multiple phone numbers", "[VcfParser]") {
+    TempMetaFile meta;
+    TempVcfFile vcf(
+        "BEGIN:VCARD\r\n"
+        "VERSION:3.0\r\n"
+        "N:Petrovic;Marko;;;\r\n"
+        "TEL:+38166123456\r\n"
+        "TEL:+38163999999\r\n"
+        "END:VCARD\r\n"
+    );
+
+    PhoneNormalizer normalizer(meta.filename());
+    VcfParser parser(normalizer, "RS");
+
+    auto contacts = parser.loadFromFile(vcf.filename());
+
+    REQUIRE(contacts.size() == 1);
+    REQUIRE(contacts[0].getPhoneNumbers().size() == 2);
+}
+
+TEST_CASE("VcfParser handles contact with no phone number", "[VcfParser]") {
+    TempMetaFile meta;
+    TempVcfFile vcf(
+        "BEGIN:VCARD\r\n"
+        "VERSION:3.0\r\n"
+        "N:Petrovic;Marko;;;\r\n"
+        "EMAIL:marko@email.com\r\n"
+        "END:VCARD\r\n"
+    );
+
+    PhoneNormalizer normalizer(meta.filename());
+    VcfParser parser(normalizer, "RS");
+
+    auto contacts = parser.loadFromFile(vcf.filename());
+
+    REQUIRE(contacts.size() == 1);
+    REQUIRE_FALSE(contacts[0].hasPhoneNumbers());
+    REQUIRE(contacts[0].getEmail() == "marko@email.com");
+}
+
+TEST_CASE("VcfParser handles contact with no email", "[VcfParser]") {
+    TempMetaFile meta;
+    TempVcfFile vcf(
+        "BEGIN:VCARD\r\n"
+        "VERSION:3.0\r\n"
+        "N:Petrovic;Marko;;;\r\n"
+        "TEL:+38166123456\r\n"
+        "END:VCARD\r\n"
+    );
+
+    PhoneNormalizer normalizer(meta.filename());
+    VcfParser parser(normalizer, "RS");
+
+    auto contacts = parser.loadFromFile(vcf.filename());
+
+    REQUIRE(contacts.size() == 1);
+    REQUIRE(contacts[0].getEmail().empty());
+}
+
+TEST_CASE("VcfParser returns empty vector for missing file", "[VcfParser]") {
+    TempMetaFile meta;
+    PhoneNormalizer normalizer(meta.filename());
+    VcfParser parser(normalizer, "RS");
+
+    auto contacts = parser.loadFromFile("fileDoesNotExist.vcf");
+
+    REQUIRE(contacts.empty());
+}
+
+TEST_CASE("VcfParser returns empty vector for empty file", "[VcfParser]") {
+    TempMetaFile meta;
+    TempVcfFile vcf("");
+
+    PhoneNormalizer normalizer(meta.filename());
+    VcfParser parser(normalizer, "RS");
+
+    auto contacts = parser.loadFromFile(vcf.filename());
+
+    REQUIRE(contacts.empty());
+}
+
+TEST_CASE("VcfParser handles contact with only name, no other fields", "[VcfParser]") {
+    TempMetaFile meta;
+    TempVcfFile vcf(
+        "BEGIN:VCARD\r\n"
+        "VERSION:3.0\r\n"
+        "N:Petrovic;Marko;;;\r\n"
+        "END:VCARD\r\n"
+    );
+
+    PhoneNormalizer normalizer(meta.filename());
+    VcfParser parser(normalizer, "RS");
+
+    auto contacts = parser.loadFromFile(vcf.filename());
+
+    REQUIRE(contacts.size() == 1);
+    REQUIRE(contacts[0].getName()    == "Marko");
+    REQUIRE(contacts[0].getSurname() == "Petrovic");
+    REQUIRE_FALSE(contacts[0].hasPhoneNumbers());
+    REQUIRE_FALSE(contacts[0].hasAddress());
+    REQUIRE(contacts[0].getEmail().empty());
+}
+
+TEST_CASE("VcfParser uses correct region for normalization", "[VcfParser]") {
+    TempMetaFile meta;
+    TempVcfFile vcf(
+        "BEGIN:VCARD\r\n"
+        "VERSION:3.0\r\n"
+        "N:Smith;John;;;\r\n"
+        "TEL:12125551234\r\n"   
+        "END:VCARD\r\n"
+    );
+
+    PhoneNormalizer normalizer(meta.filename());
+    VcfParser parser(normalizer, "US");  
+
+    auto contacts = parser.loadFromFile(vcf.filename());
+
+    REQUIRE(contacts.size() == 1);
+    REQUIRE(contacts[0].getPrimaryPhoneNumber().getNormalizedValue() == "+12125551234");
+}
